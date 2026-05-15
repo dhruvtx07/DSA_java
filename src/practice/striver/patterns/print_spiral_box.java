57f7c3269395af00ee25b17eb26a55ce3f09f195
DhruvT <dhruvtxx@gmail.com>
2026-05-15 13:56:22 +0530

    15-may-2026

--- src/practice/striver/patterns/print_spiral_box.java
+++ src/practice/striver/patterns/print_spiral_box.java
@@ -1,13 +1,13 @@
 package practice.striver.patterns;
 
 public class print_spiral_box {
-    int[][] box = new int[5][5];
+    public static void main(String[] args){
+        int[][] box = new int[5][5];
 
-    int top = 0;
-    int bottom = box.length-1;
+        int top = 0;
+        int bottom = box.length-1;
 
-    int left = 0;
-    int right = box[0].length-1;
-
-
+        int left = 0;
+        int right = box[0].length-1;
+    }
 }
